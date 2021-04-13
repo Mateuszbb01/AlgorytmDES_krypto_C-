685280b139e00ca612776fc84dd3ac30cd00675b
Mateuszbb01 <57901484+Mateuszbb01@users.noreply.github.com>
2021-04-13 12:37:55 +0200

    Add files via upload

---
+++ AlgorytmDES_C#/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tekst_szyf = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tekst_szyf
+            // 
+            this.tekst_szyf.AutoSize = true;
+            this.tekst_szyf.BackColor = System.Drawing.Color.Transparent;
+            this.tekst_szyf.Font = new System.Drawing.Font("MS Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tekst_szyf.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.tekst_szyf.Location = new System.Drawing.Point(12, 24);
+            this.tekst_szyf.Name = "tekst_szyf";
+            this.tekst_szyf.Size = new System.Drawing.Size(269, 29);
+            this.tekst_szyf.TabIndex = 0;
+            this.tekst_szyf.Text = "Kodowanie tekstu";
+            this.tekst_szyf.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 70);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(537, 119);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(71, 293);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(537, 118);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(194, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 66);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Szyfrowanie";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btn_szyf_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(339, 207);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 63);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Odszyfrowanie";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btn_odsz);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(508, 26);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Click += new System.EventHandler(this.key_txt);
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(437, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Hasło";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(679, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tekst_szyf);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Name = "Form1";
+            this.Text = "Szyfrowanie tekstu DES";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tekst_szyf;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AlgorytmDES_C#/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_odsz(object sender, EventArgs e)
+       
+        {
+            try
+            {
+                if (textBox3.Text.Length < 8)
+                {
+                    MessageBox.Show("Klucz musi się składać z 8 znaków");
+
+                    textBox3.Focus(); //przeniesienie kursora na klucz
+                    return;
+                }
+
+
+                string password = textBox3.Text;
+                string szyfrText = textBox1.Text;
+                byte[] szyfrBytes = Convert.FromBase64String(szyfrText); //Konwertuje tekst z textBox1, gdzie koduje dane binarne jako cyfry Base64, do równoważnej 8-bitowej nieoznaczonej liczby całkowitej
+
+                DESCryptoServiceProvider Alg = new DESCryptoServiceProvider();
+
+                //  8 bitowy  tajny klucz DES
+                //GetBytes  - koduje znaki w bajty
+                Alg.Key = ASCIIEncoding.ASCII.GetBytes(password); //ASCII - kodowanie znaków Unicode aby nie stracic danych które występują poza zakresem ASCII
+                Alg.IV = ASCIIEncoding.ASCII.GetBytes(password);  //wektor inicjalizacji ( IV )
+
+
+                MemoryStream txtDeciphered = new MemoryStream(); // Przechowywanie odszyfrowanych danych
+
+                CryptoStream txtCrypto = new CryptoStream(txtDeciphered, Alg.CreateDecryptor(), CryptoStreamMode.Write);  //łączenie strumieni danych z transformacjami kryptograficznymi
+
+                txtCrypto.Write(szyfrBytes, 0, szyfrBytes.Length); //przesuwanie bieżącą pozycję w strumieniu o liczbę zapisanych bajtówi zapisuje sekwencje bajtów do obecnego CryptoStream
+                txtCrypto.Close();
+                textBox2.Text = Encoding.UTF8.GetString(txtDeciphered.ToArray()); //dekodowanie sekwencji bajtów na format ciągu UTF8 i zapisanie zawartości strumienia w tablicy bajtów
+
+                txtDeciphered.Close();
+
+             
+                textBox2.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Niepoprawny klucz zabezpieczający !" + ex.Message); // wyświetlenie informacji o niepoprawnym wprowadzeniu hasła do odkodowania
+
+            }
+
+        }
+
+        private void btn_szyf_Click(object sender, EventArgs e)
+        {
+
+            if (textBox3.Text.Length < 8)
+            {
+                MessageBox.Show("Klucz musi się składać z 8 znaków");
+
+                textBox3.Focus(); //przeniesienie kursora na klucz
+                return;
+            }
+
+            try
+
+            {
+                string password = textBox3.Text;
+                string plainText = textBox1.Text;
+                byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+
+              
+                DESCryptoServiceProvider Alg = new DESCryptoServiceProvider();
+
+                //  8 bitowy  tajny klucz DES
+                Alg.Key = ASCIIEncoding.ASCII.GetBytes(password);
+                Alg.IV = ASCIIEncoding.ASCII.GetBytes(password);
+
+                MemoryStream strCiphered = new MemoryStream(); //To Store Encrypted Data
+
+                CryptoStream strCrypto = new CryptoStream(strCiphered,
+                    Alg.CreateEncryptor(), CryptoStreamMode.Write);
+
+                strCrypto.Write(plainBytes, 0, plainBytes.Length);
+                strCrypto.Close();
+                textBox2.Text = Convert.ToBase64String(strCiphered.ToArray());
+
+                strCiphered.Close();
+                textBox2.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Niepoprawny klucz zabezpieczający !" + ex.Message);
+
+            }
+        }
+
+        private void key_txt(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
